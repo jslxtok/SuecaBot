fb22cded67739118e97eb69b0e1f28a756f9088d
Heet Patel <75808215+patelheet30@users.noreply.github.com>
2023-01-30 13:19:41 +0000

    Finishes work on dealing cards, tests need to be done

--- SuecaBot/database/dbfuncs.py
+++ SuecaBot/database/dbfuncs.py
@@ -1,6 +1,8 @@
+import random
 from pathlib import Path
 import aiosqlite
 import lightbulb
+from random import shuffle
 
 dbfuncs = lightbulb.Plugin("DBFunctions")
 
@@ -37,6 +39,32 @@ async def game_join(id: str, player: str) -> str:
         return "Joined"
     else:
         return "FullGame"
+
+cards = ['AS', 'AD', 'AH', 'AC', '7S', '7D', '7H', '7C', 'KS', 'KD', 'KH', 'KC', 'JS', 'JD', 'JH', 'JC', 'QS', 'QD', 'QH', 'QC', '6S', '6D', '6H', '6C', '5S', '5D', '5H', '5C', '4S', '4D', '4H', '4C', '3S', '3D', '3H', '3C', '2S', '2D', '2H', '2C']
+
+
+def shuffle_and_return(lst: list) -> list:
+    shuffle(lst)
+    return lst
+
+
+async def dealing(id: str, player: str, dealing_cards=cards) -> str:
+    shuffled_cards = shuffle_and_return(dealing_cards)
+    trump_card = shuffled_cards[1]
+    players = await player_info(id)
+    lst_of_players = []
+    for player in players[0]:
+        lst_of_players.append(player)
+    if player != lst_of_players[0]:
+        return "NotOwner"
+    elif len(lst_of_players) != 4:
+        return "NotEnoughPlayers"
+    else:
+        async with dbfuncs.bot.d.conn.cursor() as cursor:
+            for i in range(4):
+                await cursor.execute(f"UPDATE {id} set cards = ?, trump_card = ? WHERE userid = ?", (shuffled_cards[i:i+10], trump_card, lst_of_players[i]))
+        await dbfuncs.d.conn.commit()
+        return "Dealt"
 
 
 def load(bot: lightbulb.BotApp) -> None:
